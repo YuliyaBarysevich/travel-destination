6fbe55d2abf4a52a099f1f00d4002b954f1cae20
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-02-03 12:53:34 -0800

    updates js, added css style for all pages

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 // make sure the array is in order of: 6 questions, with Mtn, City, Beach
-var imagesArray = ['mountains2', 'old-town', 'beach', 'barbeque', 'pasta2', 'seafood', 'activity', 'EducationalVacation', 'relaxFriends', 'beer', 'wine', 'coctail', 'campfire', 'club', 'sunset', 'cabin', 'plaza_hotel', 'resort'];
+var imagesArray = ['mountains2', 'old-town', 'beach', 'barbeque', 'pasta2', 'seafood', 'activity', 'EducationalVacation', 'relaxFriends', 'beer', 'wine', 'coctail', 'campfire', 'club', 'sunset', 'cabin', 'plaza_hotel', 'resort', 'mtnBoots', 'cityBoots', 'flip-flops', 'family', 'friends', 'couple'];
 
 VacationQuestion.allQuestions = [];
 
-function VacationQuestion(question, timesClickedMtn, timesClickedCity, timesClickedBeach, imageName1, imageName2, imageName3) {
+function VacationQuestion(question, timesClickedMtn, timesClickedCity, timesClickedBeach, imageName1, imageName2, imageName3, insightText) {
   this.question = question;
   this.timesClickedMtn = timesClickedMtn;
   this.timesClickedCity = timesClickedCity;
@@ -15,6 +15,7 @@ function VacationQuestion(question, timesClickedMtn, timesClickedCity, timesClic
   this.imageName1 = imageName1;
   this.imageName2 = imageName2;
   this.imageName3 = imageName3;
+  this.insightText = insightText;
   // this.hasBeenClickedArray = [];
   VacationQuestion.allQuestions.push(this);
 }
@@ -30,12 +31,14 @@ function fetchObject(key) {
 }
 
 function createVacationQuestions() {
-  new VacationQuestion('If you had the whole day to yourself, you would roam...', 0, 0, 0, 'mountains2', 'old-town', 'beach')
-  new VacationQuestion('Which food are you most likely to try ?', 0, 0, 0, 'barbeque', 'pasta2', 'seafood')
-  new VacationQuestion('What makes a trip feel truely special for you ?', 0, 0, 0, 'activity', 'EducationalVacation', 'relaxFriends')
-  new VacationQuestion('When it\`s time for a drink, you reach for ...', 0, 0, 0, 'beer', 'wine', 'coctail')
-  new VacationQuestion('When the sun goes down, what would you like to do ?', 0, 0, 0, 'campfire', 'club', 'sunset')
-  new VacationQuestion('And where would you like to stay ?', 0, 0, 0, 'cabin', 'plaza_hotel', 'resort')
+  new VacationQuestion('If you had the whole day to yourself, you would roam...', 0, 0, 0, 'mountains2', 'old-town', 'beach', 'Studies show that money spent on travel makes you happier than money spent on material goods.')
+  new VacationQuestion('Which food are you most likely to try ?', 0, 0, 0, 'barbeque', 'pasta2', 'seafood', 'Benefits of travel are almost immediate. After only a day or two, 89% of people experience significant drops in stress.')
+  new VacationQuestion('What makes a trip feel truely special for you ?', 0, 0, 0, 'activity', 'EducationalVacation', 'relaxFriends', '82% of employers think traveling makes you more employable.')
+  new VacationQuestion('When it\`s time for a drink, you reach for ...', 0, 0, 0, 'beer', 'wine', 'coctail', 'Studies show that money spent on travel makes you happier than money spent on material goods.')
+  new VacationQuestion('When the sun goes down, what would you like to do ?', 0, 0, 0, 'campfire', 'club', 'sunset', 'In 1964, San Francisco’s cable cars were named the first moving National Historic Landmark. The San Francisco cable cars are the only ones still operating in a U.S')
+  new VacationQuestion('And where would you like to stay ?', 0, 0, 0, 'cabin', 'plaza_hotel', 'resort', 'A 2010 study discovered that going on vacation doesn’t necessarily make you happier. The mere anticipation of the time off results in higher levels of reported happiness. A perfectly good reason to start planning your next vacation right now!')
+  new VacationQuestion('What shoes do you like to walk in?', 0, 0, 0, 'mtnBoots', 'cityBoots', 'flip-flops', 'Taking vacations can make you more creative and become a better problem solver.')
+  new VacationQuestion('Who are you going to travel with?', 0, 0, 0, 'family', 'friends', 'couple', 'Every country in European Union, by law, has at least four weeks of paid vacation. The United States is the only developed country in the world without a single legally required paid vacation day.')
 
 }
 createVacationQuestions();
@@ -51,6 +54,8 @@ var thisIsTheRealButtonElement = document.getElementById('thisIsTheRealButton');
 thisIsTheRealButtonElement.style.visibility = 'hidden';
 // added h4 tag - important if you do tag name you have to add index number!!!
 var h4Element = document.getElementsByTagName('h4')[0];
+var asideElement = document.getElementById ("insightsHere")
+var asidePicElement = document.getElementById('aside-pic')
 
 
 function displayVacationQuestions(indexNumber) {
@@ -58,6 +63,7 @@ function displayVacationQuestions(indexNumber) {
   leftImageElement.src = VacationQuestion.allQuestions[indexNumber].imagePath1;
   middleImageElement.src = VacationQuestion.allQuestions[indexNumber].imagePath2;
   rightImageElement.src = VacationQuestion.allQuestions[indexNumber].imagePath3;
+  asideElement.textContent = VacationQuestion.allQuestions[indexNumber].insightText;
 }
 displayVacationQuestions(0);
 imageContainer.addEventListener('click', handleImageClick);
@@ -107,7 +113,8 @@ function displayButtonNext() {
   // change text in h3
   quizQuestionsElement.textContent = 'Please click the button below to see your results';
   h4Element.style.display = 'none';
-  console.log(h4Element.textContent)
+  asideElement.style.display = 'none';
+  asidePicElement.style.display = 'none';
 }
 
 // buttonElement.addEventListener('click', submitButtonOnOff);
--- js/appResults.js
+++ js/appResults.js
@@ -18,6 +18,7 @@ var totalBeachClick = 0;
 var h3Element = document.getElementById('quizResults');
 // console.log(resurrectedDataArray[2].timesClickedMtn);
 var resultsImageElement = document.getElementById('resultsImage');
+var pElement = document.getElementById('resultsText')
 
 
 function getResults() {
@@ -28,16 +29,20 @@ function getResults() {
   }
   if (totalMtnClick > totalCityClick && totalMtnClick > totalBeachClick) {
     h3Element.textContent = "Congratulations, Mountain MAN";
-    resultsImageElement.src = "img/zion.jpeg"
+    resultsImageElement.src = "img/zion.jpeg";
+    pElement.textContent = "Zion\’s landscape is filled with rainbow-colored rock layers chiseled into sharp peaks, high mesas, and deep, twisting canyons. It\’s a place so hauntingly beautiful that you will never forget the silence of Zion\’s sandstone cathedrals, the light reflecting off the slot canyons, the rushing Virgin River, or the architect of its cliffs and canyons. Everyone should visit Zion at least once in their lifetime to hike its trails, wade through canyon waters, and watch deer graze in open meadows. Named by early settler Isaac Behunin in 1863, Zion remains true to its name—the Promised Land and a place of refuge."
   } else if (totalCityClick > totalMtnClick && totalCityClick > totalBeachClick) {
     h3Element.textContent = "Congratulations, City slicker";
     resultsImageElement.src = "img/boston.png"
+    pElement.textContent = "Boston is one of the oldest US cities. The first subway system in the US was built in Boston in 1897. The first US light house was built in the Boston Harbor in 1716. On top of all that, Boston is also where the first phone call was made. Known as the \"Cradle of Modern America\", this city is full of history and culture and it is definitely worth checking out."
   } else if (totalBeachClick > totalMtnClick && totalBeachClick > totalCityClick) {
     h3Element.textContent = "Congratulations, Beach BOY";
-    resultsImageElement.src = "img/hawaii.jpg"
+    resultsImageElement.src = "img/hawaii.jpg";
+    pElement.textContent = "From families to romance seekers, from nature lovers to adventurers, from water sports enthusiasts to hikers, from activity seekers to beach chillers: this magical chain of islands in the middle of the Pacific Ocean has it all. The 6 main islands of Hawaii are. Kauai - the Garden Isle is home to some of the most dramatic scenery in the South Pacific. Oahu - the Heart of Hawaii is home to the capital city of Honolulu and legendary surf towns like Haleiwa. Molokai - true to its island roots, Molokai is filled with rustic charm and epic beauty. Lanai - island of contradictions that balances restful luxury with rugged terrain. Maui - from its famous beaches to the peak of Haleakala, Maui offers a wealth of unforgettable experiences. Big Island of Hawaii - from active volcanoes to coffee farms and beautiful beaches to rich history, theres lots to see and do on the island of Hawaii."
   } else if (totalBeachClick == totalMtnClick || totalBeachClick == totalCityClick || totalMtnClick == totalCityClick) {
     h3Element.textContent = "Congratulations, you get to go to California !";
-    resultsImageElement.src = "img/california.jpg"
+    resultsImageElement.src = "img/cali.jpg"
+    pElement.textContent = "There are many reasons to visit California because there\’s something for everyone in the Golden State – fun, adventure, beauty, and history await you in this particular West Coast state. Of course, what would California be without its infamous weather? The weather is still great, but now the scenery and weather is just fabulous. Lather on the sunscreen and be prepared to enjoy temperatures of around 70℉ wherever you’re going. Kids and adults alike will never tire of California\’s theme parks. There’s Universal Studios Hollywood, Magic Mountain, Discovery Kingdom, Boomerang Bay and, of course, Disneyland. These are the places to go to make memories that you and your loved ones will truly never forget."
   }
   displayResults();
 }
@@ -46,6 +51,5 @@ getResults();
 
 
 function displayResults() {
-
 
 }
